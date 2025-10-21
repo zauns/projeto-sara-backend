6269f25ee2d8ab09072d41fe2e48370a41be44f4
lailasam <samaralailaa@gmail.com>
2025-10-21 11:36:50 -0300

    adicao de empresaRepository

---
+++ saraEmprega/src/main/java/sara/projeto/saraEmprega/repository/EmpresaRepository.java
@@ -0,0 +1,7 @@
+package sara.projeto.saraEmprega.repository;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import sara.projeto.saraEmprega.model.Empresa;
+
+public interface EmpresaRepository extends JpaRepository<Empresa, UUID> {}
