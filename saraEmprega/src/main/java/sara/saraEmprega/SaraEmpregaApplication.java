39f7b5deb0e30a2d356659fc365f3311aa6530fb
Davyusow <dayvsonf27@gmail.com>
2025-10-29 02:43:51 -0300

    Containers back-end e banco de dados iniciando

---
+++ saraEmprega/src/main/java/sara/saraEmprega/SaraEmpregaApplication.java
@@ -0,0 +1,25 @@
+package sara.saraEmprega;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaraEmpregaApplication {
+
+	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load(); //Lê os dados do arquivo .env da raiz do projeto
+		dotenv
+			.entries()
+			.forEach(entry -> {
+				System.setProperty(entry.getKey(), entry.getValue());
+			});
+
+		SpringApplication app = new SpringApplication(
+			SaraEmpregaApplication.class
+		);
+		app.setWebApplicationType(WebApplicationType.SERVLET);
+		app.run(args);
+	}
+}
