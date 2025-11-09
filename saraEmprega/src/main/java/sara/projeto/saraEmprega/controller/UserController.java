521ca901644963db89559784bd0f3502a0ff55ab
Davyusow <dayvsonf27@gmail.com>
2025-11-09 10:11:55 -0300

    Correção de imports

--- saraEmprega/src/main/java/sara/projeto/saraEmprega/controller/UserController.java
+++ saraEmprega/src/main/java/sara/projeto/saraEmprega/controller/UserController.java
@@ -1,22 +1,57 @@
 package sara.projeto.saraEmprega.controller;
 
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import sara.projeto.saraEmprega.dto.UserDTO;
 import sara.projeto.saraEmprega.dto.UserRequestDTO;
+import sara.projeto.saraEmprega.model.User;
+import sara.projeto.saraEmprega.ports.UserServicePort;
+import sara.projeto.saraEmprega.util.Mapper;
 
 @Validated
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/user")
-public class UserController {   
+public class UserController {
 
-    @PostMapping("/user")   //depois porta o createUser pra a classe contas controller, ela ta responsável por isso
-    public ResponseEntity<UserRequestDTO> CreateUser(@RequestBody @Valid UserRequestDTO userRequestDTO) {
-        //falta o Response o user
-        return ResponseEntity.status(HttpStatus.CREATED).body(null);
-    }
+	UserServicePort userService;
 
+	@PostMapping("/update")
+	public ResponseEntity<UserDTO> updateUser(
+		@RequestBody @Valid UserDTO userDTO,
+		Authentication auth
+	) {
+		Jwt jwt = (Jwt) auth.getPrincipal();
+		User user = userService.updateUser(userDTO, jwt.getSubject());
+		//UserDTO userResponseDTO = Mapper.mapToUserRequestDTO(user);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@PostMapping("/create")
+	public ResponseEntity<UserDTO> createUser(
+		@RequestBody @Valid UserDTO userDTO,
+		Authentication auth
+	) {
+		Jwt jwt = (Jwt) auth.getPrincipal();
+		User user = userService.CreateUser(
+			jwt.getClaim("scope"),
+			Mapper.MapToUser(userDTO)
+		);
+		UserDTO userResponseDTO = Mapper.mapToUserRequestDTO(user);
+		return new ResponseEntity<>(userResponseDTO, HttpStatus.OK);
+	}
+
+	@GetMapping("/validate-token")
+	public ResponseEntity<String> validateToken(Authentication auth) {
+		return ResponseEntity.ok("Token válido para: " + auth.getName());
+	}
 }
