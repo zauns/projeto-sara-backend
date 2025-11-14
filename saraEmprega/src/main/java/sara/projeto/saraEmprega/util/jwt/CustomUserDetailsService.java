43377da3d00834c47d1bf08e14040f1df641bae7
Davyusow <dayvsonf27@gmail.com>
2025-11-14 17:39:31 -0300

    O projeto agora esta adaptado ao padrão de projeto Port and Adapters

--- saraEmprega/src/main/java/sara/projeto/saraEmprega/util/jwt/CustomUserDetailsService.java
+++ saraEmprega/src/main/java/sara/projeto/saraEmprega/util/jwt/CustomUserDetailsService.java
@@ -7,17 +7,17 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import sara.projeto.saraEmprega.model.Conta;
-import sara.projeto.saraEmprega.ports.ContaServicePort;
+import sara.projeto.saraEmprega.repository.ContaRepository;
 
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final ContaServicePort contaService;
+    private final ContaRepository repositorio;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        Conta toReturn = contaService.buscarPorEmail(username).orElseThrow(
+        Conta toReturn = repositorio.findByEmail(username).orElseThrow(
             () -> new UsernameNotFoundException("Usuário não encontrado: " + username));
         return new ContaAutenticada(toReturn);
     }
