7fce47c61b4a40a724e78efe198167c7b128bd01
Davyusow <dayvsonf27@gmail.com>
2025-11-15 09:49:19 -0300

    Deleção das contas empresa e secretaria implementada

--- saraEmprega/src/main/java/sara/projeto/saraEmprega/controller/EmpresaController.java
+++ saraEmprega/src/main/java/sara/projeto/saraEmprega/controller/EmpresaController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,6 +53,14 @@ public class EmpresaController extends ContasController<EmpresaRequestDTO, Empre
     @PutMapping("/aprovar/{id}")
     public ResponseEntity<ContaResponseDTO> aprovarEmpresa(@PathVariable UUID id) {
         return ResponseEntity.ok(service.aprovarEmpresa(id));
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('EMPRESA') and authentication.principal.claims['userId'] == #id.toString()")
+    public ResponseEntity<Void> excluirConta(@PathVariable UUID id) {
+        service.excluirConta(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- saraEmprega/src/main/java/sara/projeto/saraEmprega/controller/SecretariaController.java
+++ saraEmprega/src/main/java/sara/projeto/saraEmprega/controller/SecretariaController.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -52,5 +53,13 @@ public class SecretariaController extends ContasController<SecretariaRequestDTO,
     @PutMapping("/aprovar/{id}")
     public ResponseEntity<ContaResponseDTO> aprovarSecretaria(@PathVariable UUID id) {
         return ResponseEntity.ok(service.aprovarSecretaria(id));
+    }
+
+    @Override
+    @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('SECRETARIA') and authentication.principal.claims['userId'] == #id.toString()")
+    public ResponseEntity<Void> excluirConta(@PathVariable UUID id) {
+        service.excluirConta(id);
+        return ResponseEntity.noContent().build();
     }
 }
