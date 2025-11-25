c0a918505bcbb70de51572aef80bfe5d3aa94f98
lailasam <samaralailaa@gmail.com>
2025-11-25 11:51:18 -0300

    .

--- saraEmprega/src/main/java/sara/projeto/saraEmprega/adapter/UserRepositoryAdapter.java
+++ saraEmprega/src/main/java/sara/projeto/saraEmprega/adapter/UserRepositoryAdapter.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
 import sara.projeto.saraEmprega.model.User;
+import sara.projeto.saraEmprega.ports.UserRepositoryPort;
 import sara.projeto.saraEmprega.repository.UserRepository;
 
 @Component
