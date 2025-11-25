77ee9b395a77c179b4d7df2a6404b1607263dbbc
Davyusow <dayvsonf27@gmail.com>
2025-11-25 20:30:28 -0300

    Ajustes Temporários

---
+++ saraEmprega/src/main/java/sara/projeto/saraEmprega/config/WebConfig.java
@@ -0,0 +1,16 @@
+package sara.projeto.saraEmprega.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
