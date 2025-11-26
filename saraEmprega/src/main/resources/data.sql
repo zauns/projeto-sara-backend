34ee31de05e5381dba0c8acbc441524cbd882578
Davyusow <dayvsonf27@gmail.com>
2025-11-25 22:28:53 -0300

    Ajustes no script.sql

--- saraEmprega/src/main/resources/data.sql
+++ saraEmprega/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 -- 1. Inserção do ADMINISTRADOR
 INSERT INTO administrador
-(id, email, senha_hash, nome, is_super_admin, endereco, telefone, tipo)
+(id, email, senha_hash, nome, is_super_admin, endereco, telefone)
 VALUES
 (
     UUID_TO_BIN(UUID()),
@@ -9,13 +9,12 @@ VALUES
     'Fulano Super Admin',
     1,
     'Rua do Alto, 100',
-    '123456789',
-    'ADMINISTRADOR'
+    '123456789'
 );
 
 -- 2. Inserção da SECRETARIA
 INSERT INTO secretaria
-(id, email, senha_hash, nome, endereco, municipio, telefone, is_validada, tipo)
+(id, email, senha_hash, nome, endereco, municipio, telefone, is_validada)
 VALUES
 (
     UUID_TO_BIN(UUID()),
@@ -25,13 +24,12 @@ VALUES
     'Rua da Secretaria, 200',
     'Recife',
     '81999991111',
-    1,
-    'SECRETARIA'
+    1
 );
 
 -- 3. Inserção da EMPRESA
 INSERT INTO empresa
-(id, email, senha_hash, nome, endereco, telefone, cnpj, biografia, links, is_validada, tipo)
+(id, email, senha_hash, nome, endereco, telefone, cnpj, biografia, links, is_validada)
 VALUES
 (
     UUID_TO_BIN(UUID()),
@@ -43,8 +41,7 @@ VALUES
     '92.167.491/0001-88',
     'Uma empresa de tecnologia focada em inovação.',
     'http://empresa-padrao.com',
-    1,
-    'EMPRESA'
+    1
 );
 
 -- 4. Inserção do USER (Candidato)
@@ -57,6 +54,5 @@ VALUES
     '$2a$04$7uuQIXKVtnEzGqFZnoPDBeaoYfeoYCgbNzrr2JuYULt9g5WBRRW0G',
     'Candidato Padrão',
     'Rua do Candidato, 500',
-    '81988887777',
-    'CANDIDATO'
+    '81988887777'
 );
