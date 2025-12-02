ab997dd966fc23abd37649358906b537a9b0197e
Davyusow <dayvsonf27@gmail.com>
2025-12-02 20:26:20 -0300

    Correção do script sql

--- saraEmprega/src/main/resources/data.sql
+++ saraEmprega/src/main/resources/data.sql
@@ -3,7 +3,7 @@ INSERT INTO administrador
 (id, email, senha_hash, nome, is_super_admin, endereco, telefone)
 VALUES
 (
-    UUID_TO_BIN(UUID()),
+    UNHEX(REPLACE(UUID(),'-','')), -- Compatível com MySQL 8 e MariaDB
     'fulanoadmnato@sara.com',
     '$2a$04$7uuQIXKVtnEzGqFZnoPDBeaoYfeoYCgbNzrr2JuYULt9g5WBRRW0G', -- admin123
     'Fulano Super Admin',
@@ -17,7 +17,7 @@ INSERT INTO secretaria
 (id, email, senha_hash, nome, endereco, municipio, telefone, is_validada)
 VALUES
 (
-    UUID_TO_BIN(UUID()),
+    UNHEX(REPLACE(UUID(),'-','')),
     'secretaria@daqui.com',
     '$2a$04$7uuQIXKVtnEzGqFZnoPDBeaoYfeoYCgbNzrr2JuYULt9g5WBRRW0G', -- admin123
     'Secretaria Padrão',
@@ -32,7 +32,7 @@ INSERT INTO empresa
 (id, email, senha_hash, nome, endereco, telefone, cnpj, biografia, links, is_validada)
 VALUES
 (
-    UUID_TO_BIN(UUID()),
+    UNHEX(REPLACE(UUID(),'-','')),
     'empresa@sara.com',
     '$2a$04$pSTzM3acOzgvkRtzwy4fMOgjqziA5g8M/6j9HugC/57zp9bJ/8Qyq', -- senha123
     'Empresa Padrão Ltda',
@@ -49,7 +49,7 @@ INSERT INTO `user`
 (id, email, senha_hash, nome, endereco, telefone)
 VALUES
 (
-    UUID_TO_BIN(UUID()),
+    UNHEX(REPLACE(UUID(),'-','')),
     'candidato.padrao@email.com',
     '$2a$04$7uuQIXKVtnEzGqFZnoPDBeaoYfeoYCgbNzrr2JuYULt9g5WBRRW0G', -- admin123
     'Candidato Padrão',
